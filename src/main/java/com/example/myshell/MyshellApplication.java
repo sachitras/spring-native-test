f3c89be11c79d91866002bf239881ff7e3f6d072
Sachitras <sachitra.samarakoon@goodhope.co>
2023-02-13 21:16:53 +0530

    Initial commit

---
+++ src/main/java/com/example/myshell/MyshellApplication.java
@@ -0,0 +1,15 @@
+package com.example.myshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshellApplication.class, args);
+		System.out.println("Hello.. Spring 3.0 and Native support!");
+
+	}
+
+}
